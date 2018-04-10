f2ba540225563e18f306d427bccf3e3c2053d330
Dustin Schau <dustinschau@gmail.com>
2018-04-09 19:15:42 -0700

    chore: tweak header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = styled.header`
 `;
 
 const Title = styled.h1`
-  font-size: 60px;
+  font-size: 36px;
   margin: 0;
   padding: 0.5rem 0;
   color: white;
@@ -20,16 +20,28 @@ const Title = styled.h1`
   font-family: Yrsa, sans-serif;
 
   text-transform: uppercase;
+
+  @media only screen and (min-width: 400px) {
+    font-size: 40px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    font-size: 60px;
+  }
 `;
 
 const Subtitle = styled.h2`
   margin: 0;
   padding: 0.25rem 0;
-  font-size: 40px;
+  font-size: 28px;
 
   border-top: 2px solid #d1dbbd;
   font-family: Yrsa, sans-serif;
   color: #d1dbbd;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 44px;
+  }
 `;
 
 function HeaderComponent() {
